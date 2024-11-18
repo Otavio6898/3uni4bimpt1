619306d36a2802ff32d50fb22d8658d7ad9aa28c
Otavio6898 <168760469+Otavio6898@users.noreply.github.com>
2024-11-18 12:07:45 -0300

    a

---
+++ app.js
@@ -0,0 +1,34 @@
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+    
+    cartao.innerHTML = `
+cartao.innerHTML = `
+}
+
+    function newFunction() {
+         { } const newLocal = <p>${resposta}</p>
+        return newLocal
+    }
+const newLocal = `
+
+let respostaEstaVisivel = false
+
+function viraCartao() {
+respostaEstaVisivel = !respostaEstaVisivel
+cartao.classList.toggle('active', respostaEstaVisivel)
+}
+
+cartao.addEventListener('click', viraCartao)
+
+
+container.appendChild(cartao)
+
+}
+
+criaCartao(
+'Lingua inglesa',
+'Como se diz OI em inglês?',
+'Oi em ingles é HI (RAI)'
+) `
